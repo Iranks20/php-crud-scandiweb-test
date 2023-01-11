4ef3d86da42038f393d402a5ff30bccd212bd14f
Kate Tampus <121474607+tkate7@users.noreply.github.com>
2023-01-10 22:41:55 -0500

    Update Insert.php
    Removed conditional statements to check for differences between product types and used polymorphism instead.

--- Classes/Crud/Insert.php
+++ Classes/Crud/Insert.php
@@ -1,72 +1,122 @@
-<?php 
+<?php
 
-    namespace Classes\Crud;
-
-    //Extend to Fetch Class to check for SKU duplicates using checkSkuDuplicate() method
-
-    class Insert extends \Classes\Crud\Fetch {
+    interface Insert {
         
-        public function insertData () {
+        public function insertData();
+        
+    }
+
+    class Dvd extends \Classes\Crud\Fetch implements Insert {
+        
+        public function insertData() {
             
-            if (isset ($_POST['save'])) {
+          if (isset($_POST['save'])) {
+              
+            $skuField = $_POST['sku'];
+            $nameField = $_POST['name'];
+            $priceField = $_POST['price'];
+            $typeField = $_POST['selector'];
+            $dvdField = $_POST['dvd'];
+              
+            $sku = !empty(trim($skuField));
+            $name = !empty(trim($nameField));
+            $price = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $priceField)));
+            $type = !empty($typeField);
+            $dvd= !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $dvdField)));
+              
+              if ($this->checkSkuDuplicate() == TRUE) {
+                  
+                  return TRUE;
+                  
+              } else if ($sku && $name && $price && $type && $dvd == TRUE) {
+                  
+                $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '$dvdField', '0', '0', '0', '0')";
+                    
+                $insert = $this->connect();
+                mysqli_query($insert, $sql);
+                  
+                }
+              
+            }
+            
+        }
+        
+    }
+
+    class Furniture extends \Classes\Crud\Fetch implements Insert {
+        
+        public function insertData() {
+          
+            if (isset($_POST['save'])) {
                 
                 $skuField = $_POST['sku'];
                 $nameField = $_POST['name'];
                 $priceField = $_POST['price'];
                 $typeField = $_POST['selector'];
-                $dvdField = $_POST['dvd'];
                 $heightField = $_POST['height'];
                 $widthField = $_POST['width'];
                 $lengthField = $_POST['length'];
-                $booksField = $_POST['books'];
-
+                
                 $sku = !empty(trim($skuField));
                 $name = !empty(trim($nameField));
                 $price = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $priceField)));
                 $type = !empty($typeField);
-                $dvd= !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $dvdField)));
-                $height = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $heightField)));-
+                $height = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $heightField)));
                 $width = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $widthField)));
                 $length = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $lengthField)));
-                $books = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $booksField)));
-                
-                //Check for any duplicate SKU
                 
                 if ($this->checkSkuDuplicate() == TRUE) {
                     
                     return TRUE;
                     
-                    //DVD
-                    
-                } else if ($sku && $name && $price && $type && $dvd == TRUE) {
-                    
-                    $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '$dvdField', '$heightField', '$widthField', '$lengthField', '$booksField')";
-                    
-                    $insert = $this->connect();
-                    mysqli_query($insert, $sql);
-                    
-                    //Furniture
-
                 } else if ($sku && $name && $price && $type && $height && $width && $length == TRUE) {
                     
-                    $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '$dvdField', '$heightField', '$widthField', '$lengthField', '$booksField')";
-                    
-                    $insert = $this->connect();
-                    mysqli_query($insert, $sql);
-                    
-                    //Books
-                    
-                } else if ($sku && $name && $price && $type && $books == TRUE) {
-                    
-                    $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '$dvdField', '$heightField', '$widthField', '$lengthField', '$booksField')";
+                    $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '0', '$heightField', '$widthField', '$lengthField', '0')";
                     
                     $insert = $this->connect();
                     mysqli_query($insert, $sql);
                     
                 }
                 
-            } 
+            }
             
+        }
+        
+    }
+
+    class Book extends \Classes\Crud\Fetch implements Insert {
+        
+        public function insertData() {
+            
+            if (isset($_POST['save'])) {
+                
+                $skuField = $_POST['sku'];
+                $nameField = $_POST['name'];
+                $priceField = $_POST['price'];
+                $typeField = $_POST['selector'];
+                $booksField = $_POST['books'];
+                
+                $sku = !empty(trim($skuField));
+                $name = !empty(trim($nameField));
+                $price = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $priceField)));
+                $type = !empty($typeField);
+                $books = !empty(trim(preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $booksField)));
+                
+                if ($this->checkSkuDuplicate() == TRUE) {
+                    
+                    return TRUE;
+                    
+                } else if ($sku && $name && $price && $type && $books == TRUE) {
+                    
+                    $sql = "INSERT INTO scandiweb (sku, name, price, type, dvd, height, width, length, books) VALUES ('$skuField', '$nameField', '$priceField', '$typeField', '0', '0', '0', '0', '$booksField')";
+                    
+                    $insert = $this->connect();
+                    mysqli_query($insert, $sql);
+                    
+                }
+                
+            }
+              
         }
         
     }
